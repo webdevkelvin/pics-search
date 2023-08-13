18296a1f150bce3cafd8e5e6cf5e0067f42f5e74
Kelvin <kel7.dev@gmail.com>
2023-08-13 11:15:31 -0400

    updating config file

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://github.io/webdevkelvin/pics-search",
+  base: "https://webdevkelvin.github.io/pics-search/",
 });
